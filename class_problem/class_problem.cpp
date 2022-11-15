37c4c4f85d03fe0c8f65fa72624690c8f896efd2
Raxan7 <manyerere201@gmail.com>
2022-11-16 00:08:14 +0300

    Exercising the use of switch statements

---
+++ class_problem/class_problem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int dayNum;
+    cin >> dayNum;
+    switch (dayNum)
+    {
+    case 0:
+        cout << "SUnday" << endl;
+        break;
+    case 1:
+        cout << "MOn" << endl;
+        break;
+    
+    default:
+        break;
+    }
+}
