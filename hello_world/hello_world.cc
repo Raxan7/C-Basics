34ea7950c979fdd42dfc96ed77aa8c73e3f78a8f
Raxan7 <manyerere201@gmail.com>
2022-11-17 23:26:45 +0300

    Added a note in the hello_world.cc file

--- hello_world/hello_world.cc
+++ hello_world/hello_world.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+/*
+    In order to change c++ into assembly code, we use 'gcc -S filename.extension'
+*/
 int main(){
     cout << "Hello World" << endl;
     return 0;
