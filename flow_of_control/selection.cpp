62deefa9b33fd8a0f027e52ad07555a29b82dcc0
Raxan7 <manyerere201@gmail.com>
2022-11-18 00:06:43 +0300

    Working with selections

---
+++ flow_of_control/selection.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 2;
+    // The if statements are executable with or without the curly brackets
+    if (x == 1)
+        cout << "Success " << endl;
+    else 
+        cout << "Not working" << endl;
+}
