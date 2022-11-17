e3f8d9729d629734781f7a00a7600e74190dfa43
Raxan7 <manyerere201@gmail.com>
2022-11-18 00:09:17 +0300

    Updated the selection.cpp file

--- flow_of_control/selection.cpp
+++ flow_of_control/selection.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int x;
+    int x, y;
     x = 2;
+    y = 3;
     // The if statements are executable with or without the curly brackets
     if (x == 1)
         cout << "Success " << endl;
     else 
         cout << "Not working" << endl;
+    
+    // This also works
+    if (y == 3){
+        cout << "Success " << endl;
+    } else {
+        cout << "Nothing " << endl;
+    }
 }
