5c55893aa80ab98b0f9c4a8887ac6c1d5941cdfc
Raxan7 <manyerere201@gmail.com>
2022-11-20 12:07:57 +0300

    A program showing the implication of the while loop in c++

---
+++ flow_of_control/while_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, i;
+    n = 0;
+    i = 1;
+    while (i < 5){
+        n += 1;
+        i++;
+    }
+    cout << "The values of n: " << n << endl;
+    cout << "The values of i: " << i << endl;
+}
