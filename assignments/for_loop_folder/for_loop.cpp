12edbeb724617ca36274a95a6d97d81648997063
Raxan7 <manyerere201@gmail.com>
2022-11-23 10:33:02 +0300

    Moved the assignments source codes into their respective folders

---
+++ assignments/for_loop_folder/for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, i;
+    n = 1;
+    for (i = 0; i < 5; i++){
+        n += i;
+    }
+    cout << n << endl;
+    cout << i << endl;
+}
