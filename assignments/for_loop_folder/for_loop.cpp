2d32cf81d5cdf3924e91cceeb643afd9128c2576
Raxan7 <manyerere201@gmail.com>
2022-11-29 14:57:43 +0300

    Prepared code for the for loop assignment

--- assignments/for_loop_folder/for_loop.cpp
+++ assignments/for_loop_folder/for_loop.cpp
@@ -1,11 +1,14 @@
+// A code to add even numbers
 #include <iostream>
 using namespace std;
 int main(){
-    int n, i;
-    n = 1;
-    for (i = 0; i < 5; i++){
-        n += i;
+    int i, n;
+    n = 0;
+    for (i = 0; i < 10; i++){
+        if (i % 2 == 0){
+            n += i;
+        }
     }
-    cout << n << endl;
-    cout << i << endl;
+    cout << "The value of n, which is the sum is : " << n << endl;
+    cout << "The value of i is : " << i << endl;
 }
