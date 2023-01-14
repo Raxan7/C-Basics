4219a78c23c2932e50161a4f0ffdd1443b1a9f1b
Raxan7 <manyerere201@gmail.com>
2023-01-15 01:26:16 +0300

    Modified the hello world.cpp

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main (){
+    int hasi = 22;
+    int chanya = 11;
+    if (hasi % chanya == 0){
+        cout << "divisible";
+    }
+    return 0;
+}
